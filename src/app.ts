3c13dc0f9cdbcb9c27a8ee3c9300a728f0150365
Neri Laredo <nerila1310@gmail.com>
2025-07-10 09:46:06 -0600

    Autenticar las peticiones

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { envs } from "./config";
 import { GithubController } from "./presentation/github/github.controller";
+import { GithubSha256Middleware } from "./presentation/middleware/github-sha256.middleware";
 
 (() => {
   main();
@@ -11,6 +12,7 @@ function main() {
   const controller = new GithubController();
 
   app.use(express.json());
+  app.use(GithubSha256Middleware.verifyGithubSignature);
 
   app.post("/api/github", controller.webhookHandler);
 
